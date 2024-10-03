94ba594785b087133697b0844a3c60eeafbe2b33
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-10-03 17:17:03 +0600

    car doctor pro added with github

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,22 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#a991f7",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+        },
       },
-    },
+      "dark",
+      "cupcake",
+    ],
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 };
